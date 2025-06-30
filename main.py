178c9feee465d049c6a077b5ee23950f72bef94a
owao <kori0423@naver.com>
2025-07-01 03:43:37 +0900

    main program file

---
+++ main.py
@@ -0,0 +1,15 @@
+import book_crawler
+import extractwords
+from pprint import pprint
+
+if __name__ == "__main__":
+    
+    sentence = input("추천받고 싶은 내용을 적어주세요.")
+    extraction = extractwords.extract_keywords_sparql(sentence)
+    keywords, query = extractwords.parse_response(extraction)
+
+    top_n = int(input("몇 권을 추천받으시겠습니까?"))
+
+    all_results = book_crawler.search_wordlist(keywords, top_n)
+
+    pprint(all_results)
